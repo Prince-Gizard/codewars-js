ce93c0f635a54e1ef414198ebfb8e534c21fa6de
Вася Бураев <buraevvasilii@gmail.com>
2022-08-07 14:06:58 +0300

    which are in

---
+++ README.md
@@ -0,0 +1,5 @@
+# My solutions tasks for [CodeWar](https://www.codewars.com/)
+
+## Stack
+
+- JavaScript
---
+++ Which are in/which are in.js
@@ -0,0 +1,11 @@
+function inArray(array1,array2){
+  let result = []
+  for (let i = 0; i < array1.length; i++){
+    for (let j = 0; j < array2.length; j++){
+      if (array2[j].includes(array1[i])){
+        result.push(array1[i])
+      }
+      }
+  }
+  return [...new Set(result)].sort()
+}
