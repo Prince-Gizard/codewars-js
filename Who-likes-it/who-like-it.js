6adffa8842f772a8ed895677fae0d0b1da885ec4
Prince-Gizard <buraevvasilii@gmail.com>
2022-09-14 14:28:35 +0300

    add another one solution

---
+++ Who-likes-it/who-like-it.js
@@ -0,0 +1,14 @@
+function likes (names) {
+  let templates = [
+    'no one likes this',
+    '{name} likes this',
+    '{name} and {name} like this',
+    '{name}, {name} and {name} like this',
+    '{name}, {name} and {n} others like this'
+  ];
+  let idx = Math.min(names.length, 4);
+  
+  return templates[idx].replace(/{name}|{n}/g, function (val) {
+    return val === '{name}' ? names.shift() : names.length;
+  });
+}
